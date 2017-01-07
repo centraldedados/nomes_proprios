825432f2af065761b6c7612027ca31793fe1a5d0
Marcos Marado <mindboosternoori@gmail.com>
2017-01-07 21:37:16 +0000

    2016: adição de regra para correcção da fonte
    No PDF-fonte para os dados de 2016, aparece o nome "Gabriele," que,
    presume-se, deveria ser apenas "Gabriele".

--- scrapers/dn/nomes-do-pdf.sh
+++ scrapers/dn/nomes-do-pdf.sh
@@ -7,4 +7,4 @@ wget http://www.dn.pt//DNMultimedia/DOCS+PDFS/nomes.pdf -o /dev/null -O nomes.pd
 pdftotext nomes.pdf -layout
 # Let's normalize this into the "nome,numero,genero" syntax used for previous sources...
 grep -v Página nomes.txt|grep -v Nomes|grep -v ^$|awk '{print $1",F,"$2}'|sed 's/\f//g'|grep -v ^,|grep -v ^$ >  ../../data/nomes-registados-2016.csv
-grep -v Página nomes.txt|grep -v Nomes|grep -v ^$|awk '{print $3",M,"$4}'|sed 's/\f//g'|grep -v ^,|grep -v ^$ >> ../../data/nomes-registados-2016.csv
+grep -v Página nomes.txt|grep -v Nomes|grep -v ^$|sed 's/Gabriele,/Gabriele/g'|awk '{print $3",M,"$4}'|sed 's/\f//g'|grep -v ^,|grep -v ^$ >> ../../data/nomes-registados-2016.csv
